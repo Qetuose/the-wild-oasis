0b33a9145ded889a6b4606af2cb7d2198d4fdedd
Qetuose <lukasbaltrimas123@gmail.com>
2024-09-30 21:40:46 +0300

    update data and password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { UpdateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: UpdateCurrentUser,
+    onSuccess: ({ user }) => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      toast.success("User account successfully updated");
+      queryClient.setQueriesData(["user"], user);
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateUser, isUpdating };
+}
